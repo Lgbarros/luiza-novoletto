920ac1945c6ce9949688b9b5c5d120272c450e61
lgbarros <lucasg.barros.94@gmail.com>
2024-03-27 22:01:05 -0300

    site profissional para a doutora luiza novoletto

---
+++ assets/js.js
@@ -0,0 +1,46 @@
+//  SCROLL TO TOP //
+$(window).scroll(function() {
+    if ($(this).scrollTop() >= 50) {        // If page is scrolled more than 50px
+        $('#return-to-top').fadeIn(200);    // Fade in the arrow
+    } else {
+        $('#return-to-top').fadeOut(200);   // Else fade out the arrow
+    }
+  });
+  $('#return-to-top').click(function() {      // When arrow is clicked
+    $('body,html').animate({
+        scrollTop : 0                       // Scroll to top of body
+    }, 100);
+});
+
+// SCROLL TO TOP WHEN CLICK ON LOGO //
+var btn = $('#logo-click');
+
+btn.on('click', function(e) {
+  e.preventDefault();
+  $('html, body').animate({scrollTop:0}, '300');
+});
+
+// FIXED MENU WHEN SCROLL //
+
+document.addEventListener("DOMContentLoaded", function(){
+		
+    window.addEventListener('scroll', function() {
+
+        if (window.scrollY > 200) {
+            document.getElementById('navbar_top').classList.add('fixed-top');
+            document.getElementById('navbar_top').classList.remove('bg-dark');
+            document.getElementById('navbar_top').classList.remove('position-absolute');
+            document.getElementById('navbar_top').classList.remove('bg-transparent');
+            // add padding top to show content behind navbar
+            navbar_height = document.querySelector('.navbar').offsetHeight;
+            document.body.style.paddingTop = navbar_height + 'px';
+        } else {
+            document.getElementById('navbar_top').classList.remove('fixed-top');
+            document.getElementById("navbar_top").classList.add("bg-dark");
+            document.getElementById('navbar_top').classList.add('position-absolute');
+            document.getElementById('navbar_top').classList.add('bg-transparent');
+             // remove padding top from body
+            document.body.style.paddingTop = '0';
+        } 
+    });
+}); 
